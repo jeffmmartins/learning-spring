1c5045a3a1a893513c5ed77e6f6ef31819145105
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-06 20:03:39 -0300

    first commit

---
+++ Produtos API/Produtos-API/src/test/java/io/github/jeffmmartins/Produtos/API/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeffmmartins.Produtos.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
