2bc5afd6b7f5fcfe36836531cc5a3b1d2d73b8bb
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-16 17:02:51 -0300

    Feat: ajustando configurações no main

---
+++ Arquitetura spring/Arquitetura-spring/src/test/java/io/github/jeffmmartins/Arquitetura/spring/ArquiteturaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeffmmartins.Arquitetura.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
