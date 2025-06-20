747b0457f68bbe5b1d4585a039dc9e8e9eef5216
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-20 17:58:37 -0300

    Feat: TodoController cirado

---
+++ Arquitetura spring/Arquitetura-spring/src/main/java/io/github/jeffmmartins/Arquitetura/spring/todos/TodoService.java
@@ -0,0 +1,20 @@
+package io.github.jeffmmartins.Arquitetura.spring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    //vai salvar o novotodo com o id já populado.
+    public TodoEntity salvar(TodoEntity novoTodo){
+        // ao ser salvo já retorna com o id populado;
+        return todoRepository.save(novoTodo);
+    }
+
+}
